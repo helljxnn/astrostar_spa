bfe156f959009962785f1e78956e75eb0bc90e43
Alejandro <alejandrogranadaperez200@gmail.com>
2025-08-26 08:13:46 -0500

    Añadiendo formulario de login para prueba de loader

--- FRONTEND/src/features/auth/pages/Login.jsx
+++ FRONTEND/src/features/auth/pages/Login.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Form } from '../components/form';
 
 function Login  ()  {
   return (
-    <div>Login</div>
+    <div className='w-1/2 h-auto grid grid-cols-1 gap-0 p-4 bg-gradient-to-r from-primary-purple to-primary-blue rounded-lg border-none' id='mainContainer'>
+      {/* Contenedor principal */}
+      <div className='grid grid-cols-2 gap-1 border-gray-800 bg-opacity-0' id='subContainer'>
+        {/* Subcontenedor */}
+        <div id='formLogin'>
+          {/* Contenedor de formulario */}
+          <Form />
+        </div>
+        <div id='imageContainer'>
+          {/* Contenedor de imagenes */}
+          
+        </div>
+      </div>
+    </div>
   );
 }
 export default Login;
