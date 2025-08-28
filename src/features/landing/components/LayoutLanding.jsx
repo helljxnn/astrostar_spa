34277b587b3d9fa5a50f75ca5851f7a919d56851
Jennx-x <jennls.prog@gmail.com>
2025-08-28 06:42:29 -0500

    Footer

--- src/features/landing/components/LayoutLanding.jsx
+++ src/features/landing/components/LayoutLanding.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import {Navbar} from './Navbar';
 import { Hero } from './Hero';
-import Footer from "./Footer";
+import { Footer } from "./Footer";
 
 
 function LayoutLanding(){
