685665bfbe48036f7916c1ad07207e7603311e9f
Alejandro <alejandrogranadaperez200@gmail.com>
2025-09-03 08:43:50 -0500

    Vacio

--- src/features/landing/components/LayoutLanding.jsx
+++ src/features/landing/components/LayoutLanding.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import {Navbar} from './Navbar';
 import { Hero } from './Hero';
-import {Footer} from "./Footer";
 import { Footer } from "./Footer";
 import { useLoading } from '../../../shared/contexts/loaderContext';
 import { Loader } from '../../../shared/components/loader';
